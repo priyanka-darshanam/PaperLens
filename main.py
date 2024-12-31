af18566773a3c164488b94e6e0eae8743caaf014
Priyanka Darshanam <priyankad1754@gmail.com>
2024-12-31 14:29:40 +0530

    Merge remote-tracking branch 'origin/master' into master

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 import streamlit as st
 import pickle
-import time
-import langchain
-from langchain import OpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
+from langchain_community.llms import OpenAI
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.schema import Document
+
 
 from dotenv import load_dotenv
 
@@ -20,32 +18,32 @@ st.title("PaperLens")
 
 st.sidebar.title("Research URLs")
 
-urls=[]
+urls = []
 for i in range(3):
-    url=st.sidebar.text_input(f"URL {i+1}")
+    url = st.sidebar.text_input(f"URL {i+1}")
     urls.append(url)
 
-process_url_clicked=st.sidebar.button("Process URLs")
-file_path="faiss_store_openai.pkl"
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_openai.pkl"
 
-main_placeholder=st.empty()
-llm=OpenAI(temperature=0.9, max_tokens=500)
+main_placeholder = st.empty()
+llm = OpenAI(temperature=0.9, max_tokens=500)
 
 if process_url_clicked:
-    loader=UnstructuredURLLoader(urls=urls)
+    loader = UnstructuredURLLoader(urls=urls)
     main_placeholder.text("Loading...")
-    data=loader.load()
+    data = loader.load()
 
-    text_splitter=RecursiveCharacterTextSplitter(
-        separators=['\n\n','\n','.',','],
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
         chunk_size=1000
     )
     main_placeholder.text("Text splitting...")
 
-    docs=text_splitter.split_documents(data)
+    docs = text_splitter.split_documents(data)
 
-    embeddings=OpenAIEmbeddings()
-    vectorstore_openai=FAISS.from_documents(docs, embeddings)
+    embeddings = OpenAIEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
     main_placeholder.text("Embedding Vector Started Building...")
 
     texts = [doc.page_content for doc in docs]
@@ -54,19 +52,19 @@ if process_url_clicked:
 
     vectorstore_openai.save_local("faiss_index")
 
-query=st.text_input("Quest: ")
+query = st.text_input("Quest: ")
 if query:
     if os.path.exists(file_path):
         with open(file_path, "rb") as f:
-            texts=pickle.load(f)
+            texts = pickle.load(f)
 
             docs = [Document(page_content=text) for text in texts]
 
             vectorstore_openai = FAISS.load_local("faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
-            retriever=vectorstore_openai.as_retriever()
+            retriever = vectorstore_openai.as_retriever()
 
-            chain=RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=retriever)
-            result=chain({"question": query}, return_only_outputs=True)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=retriever)
+            result = chain({"question": query}, return_only_outputs=True)
 
             st.header("Result")
             st.write(result["answer"])
